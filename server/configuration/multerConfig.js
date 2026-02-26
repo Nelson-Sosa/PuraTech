9cb980f0ed9bddcb32f6bdd047744e6bbe641402
Nelson-Sosa <ns5389188@gmail.com>
2026-02-26 16:29:26 -0300

    "corregir almacenamiento de imagenes a CloudinaryStorage"

--- server/configuration/multerConfig.js
+++ server/configuration/multerConfig.js
@@ -1,25 +1,15 @@
-const multer = require('multer');// Importa multer para manejar la subida de archivos
-const path = require('path');// Importa path para manejar y transformar rutas de archivo
-const fs = require('fs');
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-// Verificar si la carpeta 'uploads' existe, si no, crearla
-const uploadDir = path.join(__dirname, '..', 'uploads');
-if(!fs.existsSync(uploadDir)){
-    fs.mkdirSync(uploadDir);
-}
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "products",
+    allowed_formats: ["jpg", "png", "jpeg"],
+  },
+});
 
-// configuracion de almacenamiento del multer
-const storange = multer.diskStorage({
-    // Definición del destino donde se almacenarán los archivos
-    destination: (req, file, cb) =>{
-        cb(null, 'uploads/'); //carpeta donde se guardara las imagenes
-    },
-    // Definición del nombre con el que se guardarán los archivos
-    filename: (req, file, cb) =>{
-        cb(null, Date.now() + path.extname(file.originalname)); //nombre unico para la imagen
-    }
-})
-// Crear la instancia de multer con la configuración de almacenamiento
-const upload = multer({ storage: storange});
+const upload = multer({ storage });
 
 module.exports = upload;
