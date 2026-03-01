d2972fe89dd90fc9ac10a177de63e23a565ba94d
Nelson-Sosa <ns5389188@gmail.com>
2026-03-01 14:18:46 -0300

    "imagenes webp para mas velocidad de carga"

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -6,9 +6,9 @@ const home = () =>{
         <div className="cont">
             <h1>Bienvenido!</h1>
             <nav className="imagenes">
-                <img src="/img/gamer.png" alt="gamer" />
-                <img src="/img/Caballero.png"/>
-                <img src="/img/pc.png" alt="gamer" />
+                <img src="/img/gamer.webp" alt="gamer" loading="lazy" />
+                <img src="/img/Caballero.webp" alt="caballero" loading="lazy" />
+                <img src="/img/pc.webp" alt="pc" loading="lazy" />
             </nav>
             <div>
                 <nav>
