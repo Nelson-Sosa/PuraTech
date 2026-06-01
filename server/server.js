1cb09befa2aa5eb0394f19b0b6f23ae218c7984a
Nelson-Sosa <ns5389188@gmail.com>
2026-06-01 12:30:25 -0300

    fix: add token_usuario to allowedHeaders in CORS to unblock category fetch

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "token_usuario"]
 };
 
 const RoutesUser = require('./routes/routes');
