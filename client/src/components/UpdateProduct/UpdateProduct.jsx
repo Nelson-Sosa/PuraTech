7ede19edd32d8f943f0b92e32e540c9445307625
Nelson-Sosa <ns5389188@gmail.com>
2026-06-01 15:18:49 -0300

    fix: correct named import alias for imglyRemoveBackground in UpdateProduct

--- client/src/components/UpdateProduct/UpdateProduct.jsx
+++ client/src/components/UpdateProduct/UpdateProduct.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import '../UpdateProduct/UpdateProduct.css';
 import { API_URL } from '../../config';
-import { removeBackground } from "@imgly/background-removal";
+import { removeBackground as imglyRemoveBackground } from "@imgly/background-removal";
 
 const UpdateProduct = () => {
     const { id } = useParams();
