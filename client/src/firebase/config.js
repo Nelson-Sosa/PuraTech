4f53dd242d6a13f3a88125e0064a5341d87075fe
Nelson-Sosa <ns5389188@gmail.com>
2026-06-08 19:21:05 -0300

    feat: initialize Firebase app and export authentication services

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "demo-api-key",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "demo.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "demo",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "demo.appspot.com",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID || "000000000000",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:000000000000:web:0000000000000000"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyAOvpVqNx_Top_fQtCG_GMc1wFtFdtBYZM",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "puratech-cf4c5.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "puratech-cf4c5",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "puratech-cf4c5.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID || "562804341915",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:562804341915:web:26d3ce8302224a4f345a3e"
 };
 
 const app = initializeApp(firebaseConfig);
